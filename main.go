bbe9ae70010049ec10dfdb89c333bc87f16fd08b
afthab <mohammedafthab15@gmail.com>
2025-04-10 16:19:06 +0530

    added qr image

--- main.go
+++ main.go
@@ -6,15 +6,12 @@ import (
 )
 
 func main() {
-	// Serve the current directory (including HTML file)
 	fs := http.FileServer(http.Dir("."))
 	http.Handle("/", fs)
 
-	// Set the port for the server
 	port := "8000"
 	log.Printf("Starting server on http://localhost:%s\n", port)
 
-	// Start the server
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
